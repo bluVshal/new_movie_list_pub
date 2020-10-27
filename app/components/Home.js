e0940cf94dca4c4d4c1bb38bb5d3277656f6d508
PCPORT-PRAVISHA\bluvi <blu.vishal@gmail.com>
2020-10-27 18:02:15 +0400

    Modification of Background Image and Home screen

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
 import MovieList from '../components/MovieList';
 import { connect } from 'react-redux';
 
@@ -18,13 +18,18 @@ class Home extends Component {
     render(){
         const { movie } = this.props
         return (
-                   <FlatList
-                        data = {movie}
-                        renderItem = {({ item:movie }) => 
-                        <MovieList movie = {movie}/>
-                        }
-                        keyExtractor = {(movie) => movie.char_id}
-                   />
+                   <View style = {styles.container}>
+                        <Text style= {styles.instruction}>Please Choose A Character of Your Choice:</Text>
+                        <Image style= { styles.backgroundImage } source={require("../assets/BBadScreenBackground.jpg")}>
+                        </Image>
+                        <FlatList
+                                data = {movie}
+                                renderItem = {({ item:movie }) => 
+                                    <MovieList movie = {movie}/>
+                                }
+                                keyExtractor = {(movie) => movie.char_id}
+                        />
+                   </View>
         )
     }
 }
@@ -47,8 +52,26 @@ function mapDispatchToProps(dispatch) {
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
 
 const styles = StyleSheet.create({
-    container: {
-        fontFamily: "-apple-system, BlinkMacSystemFont Segoe UI",
-        backgroundColor: "orange"
-    }
+    backgroundImage:{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        opacity: 0.5,
+        resizeMode: 'contain',
+    },
+    container:{
+        paddingTop: '10%',
+        paddingBottom: '25%',
+        width:'100%',
+        height: '100%'
+    },
+    instruction:{
+        paddingTop: '5%',
+        paddingBottom: '5%',
+        marginLeft:'5%',
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
 })
